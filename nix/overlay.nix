0a8c1246ed8af008276da13d8495bed05fceda2f
Jacek Galowicz <jacek@galowicz.de>
2021-08-19 11:24:01 +0200

    Update override demo

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -9,16 +9,18 @@ final: prev:
   message-server = final.callPackage ../projects/message-server/build.nix { };
   message-client = final.callPackage ../projects/message-client/build.nix { };
 
-  # this is a nice example on how to override a dep of a dep of a dep...
-  message-server-nopython = prev.message-server.override {
+  # uncomment this to make postgres 13 the global postgres default lib.
+  # run nix-store -q --tree $(nix-build -A pkgs.message-server)
+  #     nix-store -q --tree $(nix-build -A pkgs.message-server)
+  # to see that they depend on that package now.
+  #postgresql = prev.postgresql_13;
+
+  # This example demonstrates how to override a dep of a dep to reach the same
+  # effect as the example before, without affecting all other packages that
+  # need postgres
+  message-server-pg13 = prev.message-server.override {
     libpqxx = prev.libpqxx.override {
-      postgresql = prev.postgresql.override {
-        gssSupport = false;
-        libxml2 = prev.libxml2.override {
-          icuSupport = false;
-          pythonSupport = false;
-        };
-      };
+      postgresql = prev.postgresql_13;
     };
   };
 }
