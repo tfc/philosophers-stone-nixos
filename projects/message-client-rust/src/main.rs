f1f6b6919e1e91c6d5833af44a80ee78f294e5cf
Stefan Hertrampf <stefan.hertrampf@cyberus-technology.de>
2021-08-26 13:51:34 +0200

    Simple postgres connection

--- projects/message-client-rust/src/main.rs
+++ projects/message-client-rust/src/main.rs
@@ -1,8 +1,12 @@
 #[macro_use]
 extern crate rocket;
+extern crate postgres;
+
+use postgres::{Client, NoTls};
 
 use std::env;
 use std::string::String;
+use std::thread;
 
 struct DbContext {
     host: String,
@@ -22,15 +26,38 @@ fn read_db_env() -> Result<DbContext, env::VarError> {
     Ok(db_context)
 }
 
+fn fetch_db(db_env: DbContext) -> String {
+    thread::spawn(move || {
+        let connect = format!(
+            "host={} user={} password={} dbname={}",
+            db_env.host, db_env.user, db_env.pass, db_env.db
+        );
+        let mut client = Client::connect(&connect, NoTls).unwrap();
+
+        for row in client
+            .query(
+                "SELECT content, date FROM testcounter ORDER BY id desc LIMIT 10",
+                &[],
+            )
+            .unwrap()
+        {
+            let content: String = row.get("content");
+            //let date: String = row.get("content");
+            println!("foo: {}", content);
+        }
+    })
+    .join()
+    .expect("Thread panicked");
+
+    "".to_string()
+}
+
 #[get("/")]
 fn index() -> String {
     let db_env = read_db_env();
 
     match db_env {
-        Ok(db) => format!(
-            "Host: {} Db: {} User: {} Pass: {}",
-            db.host, db.db, db.user, db.pass
-        ),
+        Ok(db) => fetch_db(db),
         Err(_) => "error".to_string(),
     }
 }
