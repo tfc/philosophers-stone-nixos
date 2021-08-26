623e2e758d7f0332bbad5fab0e98f4323695c8b7
Stefan Hertrampf <stefan.hertrampf@cyberus-technology.de>
2021-08-26 13:26:03 +0200

    Add rocket webserver crate

--- projects/message-client-rust/src/main.rs
+++ projects/message-client-rust/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> String {
+   "Hello World".to_string();
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
