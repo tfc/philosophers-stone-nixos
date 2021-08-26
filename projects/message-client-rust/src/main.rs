b0bc2ab2259308aab576f37da1784cadf979a90e
Stefan Hertrampf <stefan.hertrampf@cyberus-technology.de>
2021-08-26 16:58:23 +0200

    Fetch database in msg client

--- projects/message-client-rust/src/main.rs
+++ projects/message-client-rust/src/main.rs
@@ -1,8 +1,11 @@
 #[macro_use]
 extern crate rocket;
+extern crate chrono;
 extern crate postgres;
 
+use chrono::NaiveDate;
 use postgres::{Client, NoTls};
+use rocket::response::status::NotFound;
 
 use std::env;
 use std::string::String;
@@ -27,38 +30,46 @@ fn read_db_env() -> Result<DbContext, env::VarError> {
 }
 
 fn fetch_db(db_env: DbContext) -> String {
-    thread::spawn(move || {
+    let opt_result_string = thread::spawn(move || {
         let connect = format!(
             "host={} user={} password={} dbname={}",
             db_env.host, db_env.user, db_env.pass, db_env.db
         );
         let mut client = Client::connect(&connect, NoTls).unwrap();
-
-        for row in client
+        let res = client
             .query(
                 "SELECT content, date FROM testcounter ORDER BY id desc LIMIT 10",
                 &[],
             )
-            .unwrap()
-        {
-            let content: String = row.get("content");
-            //let date: String = row.get("content");
-            println!("foo: {}", content);
-        }
-    })
-    .join()
-    .expect("Thread panicked");
+            .unwrap();
 
-    "".to_string()
+        let strings: Vec<String> = res
+            .iter()
+            .map(|row| {
+                let content: String = row.get("content");
+                let date: NaiveDate = row.get("date");
+
+                format!("{}: {}", content, date)
+            })
+            .collect();
+
+        strings.join("\n")
+    })
+    .join();
+
+    match opt_result_string {
+        Ok(string) => string,
+        Err(_) => "Could not read from database".to_string(),
+    }
 }
 
 #[get("/")]
-fn index() -> String {
+fn index() -> Result<String, NotFound<String>> {
     let db_env = read_db_env();
 
     match db_env {
-        Ok(db) => fetch_db(db),
-        Err(_) => "error".to_string(),
+        Ok(db) => Ok(fetch_db(db)),
+        Err(_) => Err(NotFound("Could not find environment variables for database connection".to_string())),
     }
 }
 
