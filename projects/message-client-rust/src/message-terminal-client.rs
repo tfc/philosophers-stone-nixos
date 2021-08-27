9eba8f89bf4b824bcdd3d2c4ea8f77de416dc23f
Stefan Hertrampf <stefan.hertrampf@cyberus-technology.de>
2021-08-27 13:37:32 +0200

    Impl message-terminal-client

--- projects/message-client-rust/src/message-terminal-client.rs
+++ projects/message-client-rust/src/message-terminal-client.rs
@@ -1,3 +1,11 @@
-pub fn main() {
-    println!("Hello World");
+mod db_helper;
+use crate::db_helper::*;
+
+#[tokio::main]
+async fn main() {
+    let db_env = read_db_env().expect("Could not read database credentials");
+
+    let db_entries = fetch_db(db_env).await.expect("Could not read from database.");
+
+    println!("{}", db_entries);
 }
