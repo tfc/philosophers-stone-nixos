b388af79e48da62212cc677cf79657d235c9f7c4
Jacek Galowicz <jacek.galowicz@cyberus-technology.de>
2022-03-24 15:53:35 +0100

    Further refactoring

--- release.nix
+++ release.nix
@@ -2,52 +2,44 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import ./nix/nixpkgs.nix;
 
-  headless-msg-server-config = _: {
-    imports = [
-      ./modules/overlay.nix
-      ./modules/iso.nix
-      ./modules/size-reduction.nix
-      ./modules/stone-base.nix
-      ./modules/message-service.nix
-    ];
-  };
+  headless-msg-server-config = [
+    ./modules/overlay.nix
+    ./modules/iso.nix
+    ./modules/size-reduction.nix
+    ./modules/stone-base.nix
+    ./modules/message-service.nix
+  ];
 
-  rdp-server-config = _: {
-    imports = [
-      ./modules/overlay.nix
-      ./modules/stone-base.nix
-      ./modules/stone-desktop.nix
-    ];
-  };
+  rdp-server-config = [
+    ./modules/overlay.nix
+    ./modules/stone-base.nix
+    ./modules/stone-desktop.nix
+  ];
 in
 {
   pkgs = import ./nix/overlay.nix pkgs pkgs;
 
   headless-iso = (pkgs.nixos headless-msg-server-config).isoImage;
 
-  run-headless-vm = (pkgs.nixos [
-    headless-msg-server-config
+  run-headless-vm = (pkgs.nixos (headless-msg-server-config ++ [
     "${sources.nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
     (_: {
-      virtualisation.memorySize = "1G";
+      virtualisation.memorySize = 1024 * 1024;
       virtualisation.graphics = false;
     })
-  ]).vm;
+  ])).vm;
 
-  rdp-server-iso = (pkgs.nixos [
-    rdp-server-config
-    ./modules/iso.nix
-  ]).isoImage;
+  rdp-server-iso =
+    (pkgs.nixos (rdp-server-config ++ [ ./modules/iso.nix ])).isoImage;
 
 
-  run-rdp-server-vm = (pkgs.nixos [
-    rdp-server-config
+  run-rdp-server-vm = (pkgs.nixos (rdp-server-config ++ [
     "${sources.nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
     (_: {
-      virtualisation.memorySize = "1G";
+      virtualisation.memorySize = 1024 * 1024;
       virtualisation.graphics = true;
     })
-  ]).vm;
+  ])).vm;
 
   integration-test = pkgs.callPackage ./integration-tests/message-service.nix { };
 
